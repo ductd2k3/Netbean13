b20435a47e49267aba86c6f4547100fb28f7ac92
ductd2k3 <ductd2003@gmail.com>
2023-12-17 21:00:31 +0700

    First

---
+++ web/src/java/controller/Login.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+
+package controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+
+@WebServlet(name="Login", urlPatterns={"/login"})
+public class Login extends HttpServlet {
+   
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet Login</title>");  
+            out.println("</head>");
+            out.println("<body>");
+            String num1 = request.getParameter("num1");
+            String num2 = request.getParameter("num2");
+            String op = request.getParameter("op");
+            if(op.matches("\\+")){
+                out.println(num1 +"+"+num2 + "=" +(Double.parseDouble(num1)+Double.parseDouble(num2)));
+            }
+            out.println("</body>");
+            out.println("</html>");
+        }
+    } 
+
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        processRequest(request, response);
+    } 
+
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
